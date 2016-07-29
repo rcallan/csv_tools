418b56f53f0a5a8db091142ebe136eb2c7e16fa7
rcallan <rc7189@gmail.com>
2016-07-29 09:14:15 -0400

    removed parallel class for now

--- lib/include/csv_operations.h
+++ lib/include/csv_operations.h
@@ -13,7 +13,6 @@
 #include <vector>
 #include <algorithm>
 #include "csv_file.h"
-#include "parallel.h"
 
 using namespace std;
 
@@ -26,42 +25,6 @@ namespace csv_operations
   
   csv_file::Ptr do_arith_operation_subset(csv_file::Ptr csvFile, function<double(double, double)> myoper,
                                           unsigned firstRow, unsigned lastRow, unsigned firstCol, unsigned secondCol);
-
-  class PerformColumnOpSubsetBody : public parallel::ParallelLoopBody
-  {
-  public:
-    PerformColumnOpSubsetBody(csv_file::Ptr _csvFile, function<double(double, double)> _myoper,
-                                          unsigned _firstRow, unsigned _lastRow, unsigned _firstCol, unsigned _secondCol)
-    {
-      csvFile = _csvFile;
-      myoper = _myoper;
-      firstRow = _firstRow;
-      lastRow = _lastRow;
-      firstCol = _firstCol;
-      secondCol = _secondCol;
-
-      newNumColumns = csvFile->get_num_columns();
-    }
-
-    csv_file::Ptr csvFile; 
-    function<double(double, double)> myoper; 
-    unsigned firstRow; 
-    unsigned lastRow; 
-    unsigned firstCol; 
-    unsigned secondCol;
-
-    unsigned newNumColumns;
-
-    void operator()( const parallel::Range& range ) const
-    {
-      for (unsigned i = range.start; i < range.end; ++i)
-      {
-        csvFile->values(i, newNumColumns - 1) = to_string(myoper(stod(csvFile->values(i, firstCol)),
-                                                                  stod(csvFile->values(i, secondCol))));
-      }
-    }
-    
-  };
 
   csv_file::Ptr perform_column_op_subset(csv_file::Ptr csvFile, function<double(double, double)> myoper,
                                          unsigned firstRow, unsigned lastRow, vector<string> colsToUse);
